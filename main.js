b8b4ab5e5c198dad3b4388afd0adc89cf7a8ef2b
imac <eduard.iliashenco@gmail.com>
2016-06-14 12:01:08 +0300

    14.06

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ var http = require('http');
 var https = require('https');
 var url = require("url");
 var path = require("path");
-var paths = [];
+var paths = [], resKeys;
+var domain = 'http://ro.bunion-fix.com/';
 
 const exec = require('child_process').exec;
-exec('phantomjs index.js http://google.com/', function(error, stdout, stderr) {
+exec('phantomjs index.js http://ro.bunion-fix.com/', function(error, stdout, stderr) {
     var wpSource;
 
     if (error) {
@@ -16,16 +17,11 @@ exec('phantomjs index.js http://google.com/', function(error, stdout, stderr) {
     console.log('start to write ');
 
     if (stdout) {
-        wpSource = JSON.parse(stdout);
+        wpSource = JSON.parse(stdout, true);
     }
 
-    // fs.writeFileSync('jj.json', stdout);
     // fs.writeFileSync('indexNev.html', "<!DOCTYPE html>\n<html>\n" + wpSource.content + "\n</html>");
-    //
-    // fs.write('indexss.html', "<!DOCTYPE html>\n<html>\n" + gg[0].toString() + "\n</html>", function () {
-    //
-    // });
-    //
+
     // for (i = 1; i < wpSource.length; i++) {
     //     for (itm in wpSource[i]) {
     //         var fName  = path.basename(url.parse(itm).pathname);
@@ -34,26 +30,37 @@ exec('phantomjs index.js http://google.com/', function(error, stdout, stderr) {
     //     }
     // }
 
-    // paths = [wpSource.csss, wpSource.imgs, wpSource.jsss];
-    var outdirs = ['img', 'css', 'js'];
+    resKeys = Object.keys(wpSource);
 
-    var httpGet = (filePath) => {
-        var method = http, ext, outdir;
+    for (i = 1; i < resKeys.length; i++) {
+        for (itms in wpSource[resKeys[i]]) {
+            if (wpSource[resKeys[i]][itms].substring(0,4) != 'http') {
+                wpSource[resKeys[i]][itms] = domain + wpSource[resKeys[i]][itms];
+            }
+            httpGet(wpSource[resKeys[i]][itms]);
+        }
+    }
+
+    function httpGet(filePath) {
+        var outdirs = ['img', 'css', 'js'];
+        var method = http, outdir = 'img', ext;
         var fileName = path.basename(filePath);
 
         if (fileName.search(/\?/)) {
             fileName = fileName.split('?')[0];
         }
 
-        if (path.extname(fileName) == '') {
+        if (path.extname(fileName)) {
             ext = path.extname(fileName);
         } else {
             ext = 'none';
         }
+        
+        if (outdirs.indexOf(ext.substring(1)) >= 0) {
+            outdir = outdirs[outdirs.indexOf(ext.substring(1))]
+        }
 
-        outdir = outdirs.indexOf(ext.substring(1)) ? outdirs[outdirs.indexOf(ext.substring(1))] : outdirs[0];
-
-        if (filePath.substring(0,6) == 'https') {
+        if (filePath.substring(0,6) == 'https:') {
             method = https;
         }
 
@@ -63,11 +70,13 @@ exec('phantomjs index.js http://google.com/', function(error, stdout, stderr) {
                     console.error(`Folder with name ${outdir} already exists`);
                 }
             });
+        } else {
+            console.error(`Folder with name ${outdir} already exists`);
         }
 
         method.get(filePath, (res) => {
-            var writer = fs.createWriteStream(`${outdir}/${fileName + ext}`, {
-                flags: 'w+',
+            var writer = fs.createWriteStream(`${outdir}/${fileName}`, {
+                flags: 'w',
                 fd: null,
                 mode: 0o777,
                 autoClose: true
@@ -77,16 +86,13 @@ exec('phantomjs index.js http://google.com/', function(error, stdout, stderr) {
                 writer.write(chunk);
             });
             res.on('end', function () {
-                console.log(`Done - ${fileName + ext}`);
+                console.log(`Done - ${fileName}`);
             });
             res.on('error', function () {
-                console.error(`Error - ${fileName + ext}`);
+                console.error(`Error - ${fileName}`);
             });
         });
     };
-
-
-    // console.log(wpSource.jss[0].split('/'));
 
     console.log('DONE!');
 console.log(stderr);
